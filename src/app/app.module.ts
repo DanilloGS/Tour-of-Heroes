7d611975125db872a6a23c1d6021c338c67fcfa1
danillo <danillosouza1704@gmail.com>
2020-04-29 14:28:37 -0300

    Display Heroes List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HeroesComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
