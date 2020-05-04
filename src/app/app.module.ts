e78534fa2067760e07ab0ddf80e549a92a220069
danillo <danillosouza1704@gmail.com>
2020-05-04 12:08:13 -0300

    Routes Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,27 +7,19 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     FormsModule,
     BrowserModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        component: HeroesComponent
-      },
-      {
-        path: 'details',
-        component: HeroDetailComponent
-      }
-    ]),
     AppRoutingModule
   ],
   providers: [],
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../models/hero';
-
 import { HeroService } from '../services/hero.service';
-import { MessageService } from '../services/message.service';
  
 @Component({
   selector: 'app-heroes',
@@ -12,23 +10,13 @@ import { MessageService } from '../services/message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
   constructor( 
     private heroService: HeroService,
-    private messageService: MessageService
-  ) {
-    // Reserve the constructor for simple initialization
-    // It certainly shouldn't call a function that makes HTTP requests to a remote server as a real data service would.
-  }
+  ) { }
 
   ngOnInit(){ 
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero){
-    this.selectedHero = hero;
-    this.messageService.add('HeroService: Selected hero Id: ' + hero.id);
   }
 
   getHeroes() {
