99484e5d0ec6bb539548ba60d457060594dcbd5e
danillo <danillosouza1704@gmail.com>
2020-05-04 15:27:36 -0300

    Angular tutorial complete

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -24,4 +24,20 @@ export class HeroesComponent implements OnInit {
       .subscribe(heroes => this.heroes = heroes);
     // Async form of get the List of Heroes
   }
+
+  add(name: string) {
+    name = name.trim();
+    if(!name) {
+      return;
+    }
+    this.heroService.addHero({name} as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      })
+  }
+
+  delete(hero: Hero) {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
